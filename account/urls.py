8433634f13dc9dc21da8a4096c9f1169d28db91c
kamran-pc <kamranmaqbool>
2025-07-26 03:47:14 +0500

    added dashboard page

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import user_login
+from .views import user_login, dashboard
 
 
 urlpatterns = [
     # path('login/', user_login, name="login")
     
-    path('login/', auth_views.LoginView.as_view(), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout")
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    
+    path('dashboard/', dashboard, name="dashboard")
 ]
