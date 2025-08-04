5578f1720274875bd14fa9c1d0822b88e3b78cde
kamran-pc <kamranmaqbool>
2025-08-04 23:26:17 +0500

    added the user registration form and add url

--- account/form.py
+++ account/form.py
@@ -1,6 +1,27 @@
 from django import forms
-
+from django.contrib.auth import get_user_model
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, required=True, label='Username')
     password = forms.CharField(widget=forms.PasswordInput, required=True, label='Password')
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput
+    )
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "first_name", "email"]
+
+    # This method is calling when is_valid() method call in view.py file while registering
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["password2"]:
+            raise forms.ValidationError("Password don't match")
+        return cd["password2"]
